23e085b36b82c1752756a18cbfe5811424248ecf
Kabulzhan <kabulzhan@mail.ru>
2020-10-18 00:05:27 +0600

    Updates

---
+++ precache-manifest.7c790aae9602afa53ccac04a92374072.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52393edb4860ced5d4e5576b57adbe34",
+    "url": "/color-palettes-picker/index.html"
+  },
+  {
+    "revision": "334e3cc2afc43d238563",
+    "url": "/color-palettes-picker/static/css/2.52c0336b.chunk.css"
+  },
+  {
+    "revision": "9cc1124e10bf1e915cf3",
+    "url": "/color-palettes-picker/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "334e3cc2afc43d238563",
+    "url": "/color-palettes-picker/static/js/2.494bdbe0.chunk.js"
+  },
+  {
+    "revision": "3b7c3b25a28bb23d05f556f95ed1218a",
+    "url": "/color-palettes-picker/static/js/2.494bdbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc1124e10bf1e915cf3",
+    "url": "/color-palettes-picker/static/js/main.029010bc.chunk.js"
+  },
+  {
+    "revision": "c3548bf869aff93dec18",
+    "url": "/color-palettes-picker/static/js/runtime-main.b1487189.js"
+  },
+  {
+    "revision": "eee259aab0969b1ee06f510dc5995e48",
+    "url": "/color-palettes-picker/static/media/bg.eee259aa.svg"
+  }
+]);
